ec5648db70819252616d960152ee30adbe062529
Basir Rahimi <basiralirahimi@gmail.com>
2023-01-29 23:17:45 +0000

    scrolling text & carousel done

---
+++ src/components/Marquee.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="marquee-horizontal whitespace-nowrap overflow-hidden">
+    <h2 class="text-8xl whitespace-nowrap my-16 inline-block font-semibold" v-for="x in 4">
+      A <span class="text-outline">Reusable</span> Way of Living. &nbsp;
+    </h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+@keyframes slideLeft {
+  from {
+    transform: translateX(0);
+  }
+
+  to {
+    transform: translateX(-100%);
+  }
+}
+
+.marquee-horizontal {
+  h2 {
+    animation-name: slideLeft;
+    animation-duration: 10s;
+    animation-timing-function: linear;
+    animation-iteration-count: infinite;
+  }
+}
+</style>
