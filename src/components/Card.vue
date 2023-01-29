aab51d10cd885998aec633961812465c9a205537
Basir Rahimi <basiralirahimi@gmail.com>
2023-01-29 23:55:56 +0000

    moved card to a vue component

---
+++ src/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+  <a class="featuredCategory" :class="class" :href="link">
+    <img :src="img" :alt="imgAlt" class="block m-auto mb-5 w-4/6 max-w-xs h-auto" />
+    <p class="title uppercase font-semibold text-center">
+      {{ title }}
+    </p>
+    <p class="subtitle text-slate-500 text-center m-auto">
+      {{ subtitle }}
+    </p>
+  </a>
+</template>
+
+<script setup lang="ts">
+defineProps(['link', 'img', 'imgAlt', 'title', 'subtitle', 'class']);
+</script>
